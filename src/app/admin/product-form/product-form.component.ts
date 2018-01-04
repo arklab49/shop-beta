a94267574f3f0ee779c400c39ef71d40bc4cf8de
Arkadiusz Labudda <arkadiusz.labudda@amway.com>
2018-01-04 09:38:06 +0100

    Implementing deleting product functionality

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -33,7 +33,14 @@ export class ProductFormComponent implements OnInit {
     } else {
       this.productService.create(product);
     }
-      this.router.navigate(['/admin/products']);
+    this.router.navigate(['/admin/products']);
+  }
+
+  delete() {
+    if (!confirm('Are you sure you want to delete this product')) return;
+
+    this.productService.delete(this.id);
+    this.router.navigate(['/admin/products']);
   }
 
   ngOnInit() {
